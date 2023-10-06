a90692d238425f27056d5b7f488ad7fcc35fcc68
cosato003 <142634270+cosato003@users.noreply.github.com>
2023-10-06 07:37:04 -0500

    faltan tipos de lux y num de luces

--- src/examen.java
+++ src/examen.java
@@ -1,16 +1,17 @@
 import java.util.Random;
-import java.util.ArrayList;
 
 public class examen {
     // funciono para calcula un numero max y un min aleatorio
     public static int generarAleatorio(int min, int max) {
-        Random random = new Random();
-        return random.nextInt(max - min + 1) + min;
+        Random random = new Random(); // variable para generar numeros aleatorios
+        return random.nextInt(max - min + 1) + min; // genera numeros aleatorios
 
     }
 
     // funcion para calcula el costo de luz de un hotel
     public static double calcularCostoLuz(int tipoLuz, int horasEncendidas) {
+
+        // inicia el costo por hora en 0.0
         double costoPorHora = 0.0;
 
         switch (tipoLuz) {
@@ -30,6 +31,21 @@ public class examen {
     public static void main(String[] args) {
         int numHoteles = generarAleatorio(1, 7);
         System.out.println("Numero de hoteles: " + numHoteles);
+
+        for (int i = 0; i < numHoteles; i++) {
+            int numPisos = generarAleatorio(3, 15);
+            int tipoLuz = generarAleatorio(1, 3);
+            int horasEncendidas = generarAleatorio(0, 24);
+
+            double costoLuz = calcularCostoLuz(tipoLuz, horasEncendidas);
+
+            System.out.println("Hotel(es) " + (i + 1));
+            System.out.println("Numero de pisos " + numPisos);
+            System.out.println("Tipo de luz " + tipoLuz);
+            System.out.println("Horas encendidias " + horasEncendidas);
+            System.out.println("Costo de la luz " + costoLuz);
+
+        }
     }
 
 }
